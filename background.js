55b00f1d051360d7d85f77b7d84e85f4c683f30e
mr.infinidy <afkdev8@gmail.com>
2021-01-11 14:30:40 +0100

    add-on now compatible with firefox

--- background.js
+++ background.js
@@ -10,5 +10,5 @@ browser.contextMenus.onClicked.addListener(function(clickedItem) {
     var searchTerm = encodeURI(clickedItem.selectionText);
     searchTerm = searchTerm.replace(/\#/g, '%23');
     var address = "https://www.youtube.com/results?search_query=" + searchTerm;
-    browser.tabs.create(address);
+    browser.tabs.create({url:address});
 })
