f828e360fa058e7d8e426a104ae059e0ac6349d4
afkdev8 <afkdev8@gmail.com>
2022-01-28 00:43:02 +0100

    old commit

--- background.js
+++ background.js
@@ -9,6 +9,7 @@ browser.contextMenus.create(searchThis);
 browser.contextMenus.onClicked.addListener(function(clickedItem) {
     var searchTerm = encodeURI(clickedItem.selectionText);
     searchTerm = searchTerm.replace(/\#/g, '%23');
+    searchTerm = searchTerm.replace(/\&/g, '%26');
     var address = "https://www.youtube.com/results?search_query=" + searchTerm;
     browser.tabs.create({url:address});
 })
